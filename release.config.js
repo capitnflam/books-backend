cb0d622e0e0cbd703d9b1d1714af14390fbfe1cb
capitnflam <capitnflam@gmail.com>
2024-03-21 00:27:16 +0100

    feat(ci): use common release config (#4)

--- release.config.js
+++ release.config.js
@@ -1,16 +1,7 @@
-const customTransform = require('./tools/commit-transform')
-const plugins = require('./tools/plugins')
-
 /**
  * @type {import('semantic-release').Options}
  */
 module.exports = {
   branches: ['main'],
-  releaseRules: plugins[0][1].releaseRules,
-  parserOpts: {
-    mergePattern: /^Merge pull request #(\d+) from (.*)$/,
-    mergeCorrespondence: ['id', 'source'],
-  },
-  writerOpts: { transform: customTransform },
-  plugins,
+  extends: ['@flaminc/semantic-release-configs/github-publish'],
 }
